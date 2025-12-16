5819f646a40c49dcc3f70733266c878e129a28ac
usrlocal <usrlocal@GA213-FE18.ad.univ-angers.fr>
2025-12-16 16:10:20 +0100

    feat: Ajout des routes articles et liaison Frontend pour l'affichage des actus

---
+++ backend/routes/articles.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+// Assurez-vous que le nom du fichier correspond (Article.js ou Articles.js)
+const Article = require('../models/Article'); 
+
+// ==========================================================
+// ➡️ ROUTE : GET /api/articles
+// Récupérer tous les articles (Public)
+// ==========================================================
+router.get('/', async (req, res) => {
+    try {
+        // On récupère les articles et on les trie du plus récent au plus ancien (-1)
+        const articles = await Article.find().sort({ date_publication: -1 });
+        res.json(articles);
+    } catch (error) {
+        console.error("Erreur récupération articles:", error);
+        res.status(500).json({ msg: "Erreur serveur" });
+    }
+});
+
+module.exports = router;
